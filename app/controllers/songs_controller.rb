daae7baa71398f6deaaa305f48aa06ae65f6e143
danielsun <danielsun12345@gmail.com>
2020-07-15 16:33:06 -0700

    fixing routes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,33 +1,72 @@
 class SongsController < ApplicationController
 
     def index
-
+        @songs = Song.all
     end 
 
     def new
+        @song = Song.new
 
     end 
 
     def create
-
+        @song = Song.new(song_params)
+        if @song.save
+            redirect_to song_path(@song)
+        else 
+            render :new
+        end 
     end 
 
 
     def show 
-
+        @song = Song.find_by(id: params[:id])
+        if @song
+            render :show
+        else 
+            redirect_to songs_path
+        end 
     end 
 
     def edit
-
+        @song = Song.find_by(id: params[:id])
+        if @song
+            render :edit
+        else 
+            redirect_to songs_path
+        end 
     end 
 
     def update 
-
+        @song = Song.find_by(id: params[:id])
+        if @song
+            @song.update(song_params)
+            redirect_to song_path(@song)
+        else
+            redirect_to songs_path
+        end 
     end 
     
 
     def destroy
-
+        @song = Song.find_by(id: params[:id])
+        if @song 
+            @song.destroy
+            redirect_to songs_path
+        else
+            redirect_to songs_path
+        end 
     end 
+
+private 
+    
+    def song_params
+        params.require(:song).permit(
+            :title,
+            :artist,
+            :genre
+        )
+    end 
+
 
 end 
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_15_220918) do
+ActiveRecord::Schema.define(version: 2020_07_15_221729) do
 
   create_table "songs", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
     t.string "artist"
     t.string "genre"
-    t.string "release_date"
-    t.string "user_id"
   end
 
 end
