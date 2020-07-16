35c3ce79e16ff45ccf0db20cce5d100794fbcbb7
danielsun <danielsun12345@gmail.com>
2020-07-16 00:31:05 -0700

    user edits

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,22 @@ Rails.application.routes.draw do
   
   resources :playlists 
   resources :songs
-  get '/', to: 'users#new', as: 'user'
-  post '/', to: 'users#create'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  get '/:user', to: 'users#show', as: 'homepage'
+  resources :users
+  resources :session
+  # get '/', to: 'users#new', as: 'user' 
+  # post '/', to: 'users#create'
+  # get '/login', to: 'sessions#new'
+  # post '/login', to: 'sessions#create'
+  # get '/:user', to: 'users#show', as: 'homepage'
 
+
+  # get '/users/new', to: 'users#new', as: 'new_user'
+  # get '/users', to:'users#index', as: 'users'
+  # post '/users', to:'users#create'
+  # get '/users/:id', to: 'users#show', as: 'user'
+  # get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  # get '/signin', to: 'session#new', as: 'signin'
+  # post '/session', to: 'session#create', as: 'session'
+  # delete '/session/', to: 'session#destroy'
+  
 end
